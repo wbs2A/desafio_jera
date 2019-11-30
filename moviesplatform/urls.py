178309ef61d8ee153b6c71c3fead3636cc4b4631
Wesley Barbosa <34480299+wbs2a@users.noreply.github.com>
2019-11-30 18:45:58 -0400

    Movie search by string and Movie Detail

--- moviesplatform/urls.py
+++ moviesplatform/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage, search, show_movie
 
 urlpatterns = [
     path('', homepage, name='homepage'),
+    path('busca/', search, name='search_movie'),
+    path('filme/<int:movieid>', show_movie, name='movie_info'),
 
 ]
--- moviesplatform/views.py
+++ moviesplatform/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.utils import translation
-from django.conf import settings
-import requests, random
 import json
+import requests
+from django.conf import settings
+from urllib.parse import urlencode
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import translation
 
 
 def get_banner_movie(_id, headers, query):
@@ -39,7 +40,7 @@ def homepage(request):
     except:
         page = 1
     #conjunto de urls identificadas que serão utilizadas da API
-    urls = [{'name': 'movies', 'url': 'https://api.themoviedb.org/3/movie/popular','query': {'sort_by': 'popularity.desc', "page": page}},{'name': 'genre', 'url': 'https://api.themoviedb.org/3/genre/movie/list', 'query': {}}]
+    urls = [{'name': 'movies', 'url': 'https://api.themoviedb.org/3/movie/popular','query': {'sort_by': 'popularity.desc', "page": page}}]
     #Parâmetros de busca a serem passados para a API
     query = {"api_key": settings.TMDB_API_KEY, "language": translation.get_language()}
 
@@ -49,7 +50,6 @@ def homepage(request):
     json_data = get_popular_movies(urls, headers, query)
 
     context = {"movies": json_data['movies']['results'],
-               'genres': json_data['genre']['genres'],
                'page': json_data['movies']['page'],
                'total_pages': json_data['movies']['total_pages'],
               }
@@ -59,3 +59,23 @@ def homepage(request):
         context['recomendation'] = json_data['recomend']['results'][0]['key']
 
     return render(request, template_name='moviesplatform/homepage.html', context=context)
+
+
+def search(request):
+    try:
+        page = int(request.GET['page'])
+    except:
+        page = 1
+    url = 'https://api.themoviedb.org/3/search/movie'
+    query = {"api_key": settings.TMDB_API_KEY, "language": translation.get_language(), 'query': request.POST.get('search_query'), "page": page}
+    response = requests.request("GET", url, params=query)
+    json_data = json.loads(response.text)
+    context = {'result': json_data['results'],'page': json_data['page'], 'total_pages': json_data['total_pages']}
+    return render(request, template_name='moviesplatform/results.html', context=context)
+
+def show_movie(request, movieid):
+    url = 'https://api.themoviedb.org/3/movie/'+str(movieid)
+    query = {"api_key": settings.TMDB_API_KEY, "language": translation.get_language()}
+    response = requests.request("GET", url, params=query)
+    context = {'movie': json.loads(response.text)}
+    return render(request, template_name='moviesplatform/movieinfo.html', context=context)
